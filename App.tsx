4f85a220d1a0ec585f6566b004e4b26f0d9b5f18
Erick Vergara <erickandres30@gmail.com>
2020-08-05 23:31:44 -0500

    Configuracion de redux

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import Navigation from "./src/navigations/Navigation";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import Reducers from "./src/reducers";
 
 export default function App() {
-    return <Navigation />;
+    return (
+        <Provider store={createStore(Reducers)}>
+            <Navigation />
+        </Provider>
+    );
 }
---
+++ src/reducers/userReducer.ts
@@ -0,0 +1,6 @@
+const userTest = {
+    name: "Erick Vergara",
+    email: "erickandres30@gmail.com",
+};
+
+export default () => userTest;
