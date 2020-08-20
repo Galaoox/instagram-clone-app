3276aa8e8d31798084e4d90c1a7bdfe193d1aa49
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2020-08-19 23:55:30 -0500

    Ajustes de estilos para que el boton aparesca desactivado cuando disabled sea true

--- src/components/Header/CloseButton.tsx
+++ src/components/Header/CloseButton.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { TouchableOpacity, StyleSheet } from "react-native";
 import { Icon } from "react-native-elements";
+import { colors } from "../../utils/theme";
 
 interface IHeaderButtonProps {
     name: string;
     color?: string;
     onPress: Function;
+    disabled?: boolean;
 }
 
 export default function HeaderButton(props: IHeaderButtonProps) {
-    const { name, color, onPress } = props;
+    const { name, color, onPress, disabled = false } = props;
 
     return (
-        <TouchableOpacity onPress={() => onPress()}>
+        <TouchableOpacity onPress={() => onPress()} disabled={disabled}>
             <Icon
                 name={name}
                 type="material-community"
-                color={color}
+                color={disabled ? colors.iconDisable : color}
                 iconStyle={styles.closeHeader}
             />
         </TouchableOpacity>
